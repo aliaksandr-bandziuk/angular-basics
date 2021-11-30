b748d6c5477beded3fde235afd7af35d0ce50e79
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2021-12-01 00:42:22 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { PostComponent } from './post/post.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Post2Component } from './post2/post2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent,
+    Post2Component
+   ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
