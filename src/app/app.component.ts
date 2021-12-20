68cc869ca3504337fad964bd040c0043b081200a
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2021-12-21 00:13:14 +0300

    learn stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
-import { map, filter } from 'rxjs/operators'
+import { Subscription, Observable } from 'rxjs';
 
 
 @Component({
@@ -13,16 +12,31 @@ export class AppComponent {
   sub: Subscription
 
   constructor() {
-    const intervalStream$ = interval(1000)
+    const stream$ = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(1)
+      }, 1500)
 
-    this.sub = intervalStream$
-      .pipe(
-        filter(value => value % 2 === 0),
-        map((value) => `Mapped value: ${value}`)
-      )
-      .subscribe((value) => {
-        console.log('value: ', value);
+
+      setTimeout(() => {
+        observer.complete()
+      }, 1900)
+
+      setTimeout(() => {
+        observer.error('Something went wrong')
+      }, 2000)
+
+      setTimeout(() => {
+        observer.next(2)
+      }, 2500)
     })
+
+    this.sub = stream$
+      .subscribe(
+        value => console.log('Next: ',value),
+        error => console.log('Error: ', error),
+        () => console.log('Complete')
+      )
   }
 
   // останавливает стрим intervalStream$
