3fdeaad5e3f04defc5bb166195e5e314636790ed
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-01-18 17:38:33 +0300

    services is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { PostComponent } from './post/post.component';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostComponent,
-    Post2Component
+    AppComponent
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
